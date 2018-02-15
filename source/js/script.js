5a6272b1b9d4c955a7598d653e594238657cc506
randomrationalizer <lifelower@gmail.com>
2018-02-15 22:48:35 +0300

    Оживляет слайдеры на главной Реализовано пролистывание слайдеров в блоках Отзывы и Тарифы

--- source/js/script.js
+++ source/js/script.js
@@ -35,3 +35,66 @@ if (document.querySelector(".map")) {
     });
   }
 }
+
+//Слайдер в блоке с отзывами
+var slideNumber = 1;
+showDivs(slideNumber);
+
+function plusDivs(n) {
+  showDivs(slideNumber += n);
+}
+
+function currentDiv(n) {
+  showDivs(slideNumber = n);
+}
+
+function showDivs(n) {
+  var i;
+  var slides = document.getElementsByClassName("slider__item");
+  var toggles = document.getElementsByClassName("slider__toggle");
+  if (n > slides.length) {slideNumber = 1}
+  if (n < 1) {slideNumber = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < toggles.length; i++) {
+    toggles[i].className = toggles[i].className.replace(" slider__toggle--active", "");
+  }
+  slides[slideNumber-1].style.display = "flex";
+  toggles[slideNumber-1].className += " slider__toggle--active";
+}
+
+//Слайдер в блоке с тарифами
+if (document.querySelector(".price-list")) {
+  var table = document.querySelector(".price-list__table");
+  var toggleBase = document.querySelector(".price-list__toggle--base");
+  var toggleStd = document.querySelector(".price-list__toggle--std");
+  var toggleUnlim = document.querySelector(".price-list__toggle--unlim");
+
+  toggleBase.addEventListener("click", function (evt) {
+    var toggleActive = document.querySelector(".price-list__toggle--active");
+    toggleActive.classList.remove("price-list__toggle--active");
+    toggleBase.classList.add("price-list__toggle--active");
+    table.classList.remove("price-list__table--translate-33");
+    table.classList.remove("price-list__table--translate-66");
+    table.classList.add("price-list__table--translate-0");
+  });
+
+  toggleStd.addEventListener("click", function (evt) {
+    var toggleActive = document.querySelector(".price-list__toggle--active");
+    toggleActive.classList.remove("price-list__toggle--active");
+    toggleStd.classList.add("price-list__toggle--active");
+    table.classList.remove("price-list__table--translate-0");
+    table.classList.remove("price-list__table--translate-66");
+    table.classList.add("price-list__table--translate-33");
+  });
+
+  toggleUnlim.addEventListener("click", function (evt) {
+    var toggleActive = document.querySelector(".price-list__toggle--active");
+    toggleActive.classList.remove("price-list__toggle--active");
+    toggleUnlim.classList.add("price-list__toggle--active");
+    table.classList.remove("price-list__table--translate-0");
+    table.classList.remove("price-list__table--translate-33");
+    table.classList.add("price-list__table--translate-66");
+  });
+}
